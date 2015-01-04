d6402f821f27f24116b38975d9a6bb0924c6b93f
rmens <r.mens@me.com>
2015-01-04 20:21:06 +0100

    TODO's added. I'll leave this for now.

--- audiologger.sh
+++ audiologger.sh
@@ -3,3 +3,8 @@ find $_OUTPUTLOCATION* -mtime +$_MAXAGE -exec rm {} \;
 
 # Audiologger starten
 wget $_STREAM --output-document=$_OUTPUTLOCATION/$_DATE.mp3 --user-agent="Audiologger ZWFM"
+
+# TODO: Manier vinden om ieder uur af te breken (cronjob)
+# TODO: Beetje logging?
+# TODO: Wat als connectie halverwege faalt?
+# TODO: Wat als dit al op xx:xx:59 sec gestart wordt, verkeerd uur in de naam dan...
