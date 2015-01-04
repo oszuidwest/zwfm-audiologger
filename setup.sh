95730cbb106775c95de4d7c91ff233df36af85be
rmens <r.mens@me.com>
2015-01-04 20:17:37 +0100

    Timezone + nep sync toegevoegd

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 #initial setup, centos 6
 
+#Timezone goed zetten (werkt mogelijk niet op OpenVZ)
+cp /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
+
+#Yummin' some stuff
 yum update -y
 yum install ntpdate
-ntpdate ntp.xs4all.nl
 yum install httpd 
 yum install wget 
+
+#Datum en tijd goedzetten
+ntpdate ntp.xs4all.nl
