f20c843c033dc95b52876761919627723d50f526
rmens <r.mens@me.com>
2015-01-04 20:08:28 +0100

    Comments in config.sh verbeteren

--- config.sh
+++ config.sh
@@ -3,4 +3,4 @@
 _OUTPUTLOCATION=/var/www/html #in welke map moeten de bestanden komen?
 _STREAM=http://icecast.zuidwestfm.nl/zuidwest.mp3 #link naar icecast- of shoutcast-stream
 _MAXAGE=14 #in dagen
-_DATE=date +"%m-%d-%Y_%Hu"
+_DATE=date +"%m-%d-%Y_%Hu" #poept de datum en uur uit. Bijvoorbeeld: "01-04-2015_20u" (via: http://www.thegeekstuff.com/2013/05/date-command-examples/)
