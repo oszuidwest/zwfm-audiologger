69341b894eadc27f68d225d7a34896c59d2491fa
rmens <r.mens@me.com>
2015-03-28 16:41:19 +0100

    tidy

--- config.sh
+++ config.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 ## Configuratie 
-#
 STREAM='http://icecast.zuidwestfm.nl/zuidwest.mp3'
 SCRIPTROOT=/logger
 TMPDIR=/logger/tmp
@@ -9,6 +8,6 @@ LOGDIR=/logger/logs
 #poept de datum en uur uit. Hier bijv: "01-04-2015_20u" (via: http://www.thegeekstuff.com/2013/05/date-command-examples/)
 TIMESTAMP=$(date +"%m-%d-%Y_%Hu")
 USER=logger
-#Change the value below to adjust the number of days to keep the logs for (42 is default in line with regulations)
+
+# Hoeveel dagen moet de audio bewaard blijven
 KEEP=14
-##
