d349f14df0465f661d0a2364d9b731b36dc376bb
Raymon Mens <r.mens@me.com>
2015-04-19 22:30:41 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -3,7 +3,7 @@ STREAMURL='http://icecast.zuidwestfm.nl/zuidwest.mp3'
 PATH=/root/audiologger
 LOGDIR=/usr/share/icecast/web/audiologger
 #poept de datum en uur uit. Hier bijv: "01-04-2015_20u" (via: http://www.thegeekstuff.com/2013/05/date-command-examples/)
-TIMESTAMP=$(date +"%m-%d-%Y_%Hu")
+TIMESTAMP=$(/bin/date +"%m-%d-%Y_%Hu")
 
 # Hoeveel dagen moet de audio bewaard blijven
 KEEP=14
