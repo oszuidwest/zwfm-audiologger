cd98fb6dd385cd1beb7ee51e6e6e6bb26c1628f5
Raymon Mens <r.mens@me.com>
2015-04-19 23:16:02 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ cp /usr/share/zoneinfo/Europe/Amsterdam /etc/localtime
 ## Yummin' some stuff
 yum update -y
 yum install ntpdate -y
-yum install wget -y
 
 ## Datum en tijd goedzetten
 ntpdate ntp.xs4all.nl
