e42dda0501775e678fab0abd16fda5430a55745c
rmens <r.mens@me.com>
2024-05-14 22:37:02 +0200

    Extend

--- audiologger.sh
+++ audiologger.sh
@@ -4,6 +4,7 @@
 STREAMURL='https://icecast.zuidwestfm.nl/zuidwest.mp3'
 RECDIR='/var/audio'
 LOGFILE='/var/log/audiologger.log'
+METADATA_URL='https://www.zuidwestupdate.nl/wp-json/zw/v1/broadcast_data'
 # Output date and hour, e.g., "2023_12_31_20u"
 TIMESTAMP=$(/bin/date +"%Y-%m-%d_%Hu")
 # Number of days to keep the audio files
@@ -22,6 +23,16 @@ pids=$(pgrep -f "$STREAMURL")
 if [ -n "$pids" ]; then
     kill -9 $pids || { echo "$(date): Failed to kill processes: $pids" >> "$LOGFILE"; exit 1; }
 fi
+
+# Fetch current program name from fm.now
+program_name=$(curl --silent "$METADATA_URL" | jq -r '.fm.now')
+if [ -z "$program_name" ] || [ "$program_name" == "null" ]; then
+    echo "$(date): Failed to fetch current program name or program name is null" >> "$LOGFILE"
+    program_name="Unknown Program"
+fi
+
+# Write metadata to a file
+echo "Current Program: $program_name" > "${RECDIR}/${TIMESTAMP}.metadata" || { echo "$(date): Failed to write metadata file" >> "$LOGFILE"; exit 1; }
 
 # Record next hour's stream
 wget --quiet --background --user-agent="Audiologger ZuidWest (Debian 11)" -O "${RECDIR}/${TIMESTAMP}.mp3" "$STREAMURL" > /dev/null 2>&1 || { echo "$(date): Failed to start recording from $STREAMURL" >> "$LOGFILE"; exit 1; }
