cf4953dd82cb00a4eaaf5fc47bfb0a623d1e3c98
rmens <r.mens@me.com>
2024-05-14 22:39:41 +0200

    More robust + meta

--- audiologger.sh
+++ audiologger.sh
@@ -10,6 +10,17 @@ TIMESTAMP=$(/bin/date +"%Y-%m-%d_%Hu")
 # Number of days to keep the audio files
 KEEP=31
 
+# Check if jq and wget are installed
+if ! command -v jq &> /dev/null; then
+    echo "$(date): jq is not installed" >> "$LOGFILE"
+    exit 1
+fi
+
+if ! command -v wget &> /dev/null; then
+    echo "$(date): wget is not installed" >> "$LOGFILE"
+    exit 1
+fi
+
 # Create recording directory if it does not exist
 if [ ! -d "$RECDIR" ]; then
     mkdir -p "$RECDIR" || { echo "$(date): Failed to create directory: $RECDIR" >> "$LOGFILE"; exit 1; }
@@ -19,20 +30,20 @@ fi
 find "$RECDIR" -type f -mtime "+$KEEP" -exec rm {} \; || { echo "$(date): Failed to remove old files in $RECDIR" >> "$LOGFILE"; exit 1; }
 
 # Kill processes from the previous hour associated with the stream URL
-pids=$(pgrep -f "$STREAMURL")
-if [ -n "$pids" ]; then
-    kill -9 $pids || { echo "$(date): Failed to kill processes: $pids" >> "$LOGFILE"; exit 1; }
+PIDS=$(pgrep -f "$STREAMURL")
+if [ -n "$PIDS" ]; then
+    kill -9 $PIDS || { echo "$(date): Failed to kill processes: $PIDS" >> "$LOGFILE"; exit 1; }
 fi
 
-# Fetch current program name from fm.now
-program_name=$(curl --silent "$METADATA_URL" | jq -r '.fm.now')
-if [ -z "$program_name" ] || [ "$program_name" == "null" ]; then
+# Fetch current program name from the API
+PROGRAM_NAME=$(curl --silent "$METADATA_URL" | jq -r '.fm.now')
+if [ -z "$PROGRAM_NAME" ] || [ "$PROGRAM_NAME" == "null" ]; then
     echo "$(date): Failed to fetch current program name or program name is null" >> "$LOGFILE"
-    program_name="Unknown Program"
+    PROGRAM_NAME="Unknown Program"
 fi
 
 # Write metadata to a file
-echo "Current Program: $program_name" > "${RECDIR}/${TIMESTAMP}.metadata" || { echo "$(date): Failed to write metadata file" >> "$LOGFILE"; exit 1; }
+echo "$PROGRAM_NAME" > "${RECDIR}/${TIMESTAMP}.metadata" || { echo "$(date): Failed to write metadata file" >> "$LOGFILE"; exit 1; }
 
 # Record next hour's stream
-wget --quiet --background --user-agent="Audiologger ZuidWest (Debian 11)" -O "${RECDIR}/${TIMESTAMP}.mp3" "$STREAMURL" > /dev/null 2>&1 || { echo "$(date): Failed to start recording from $STREAMURL" >> "$LOGFILE"; exit 1; }
+wget --quiet --background --user-agent="Audiologger ZuidWest (2024.05)" -O "${RECDIR}/${TIMESTAMP}.mp3" "$STREAMURL" > /dev/null 2>&1 || { echo "$(date): Failed to start recording from $STREAMURL" >> "$LOGFILE"; exit 1; }
